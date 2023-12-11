0dba4f2fcce523b42f681734e5df3dda04be0e85
Abderrahmane <aboussororabderrahmane@gmail.com>
2023-12-11 11:58:27 +0100

    CA-21

--- src/main/java/com/AboussororAbderrahmane/aftas/services/interfaces/ILevelService.java
+++
@@ -1,7 +0,0 @@
-package com.AboussororAbderrahmane.aftas.services.interfaces;
-
-import com.AboussororAbderrahmane.aftas.dtos.LevelDTO;
-import com.AboussororAbderrahmane.aftas.services.IData;
-
-public interface ILevelService extends IData<LevelDTO, LevelDTO, Integer> {
-}
--- src/main/java/com/AboussororAbderrahmane/aftas/services/interfaces/IMemberService.java
+++
@@ -1,8 +0,0 @@
-package com.AboussororAbderrahmane.aftas.services.interfaces;
-
-import com.AboussororAbderrahmane.aftas.dtos.member.MemberDTO;
-import com.AboussororAbderrahmane.aftas.dtos.member.RequestMemberDTO;
-import com.AboussororAbderrahmane.aftas.services.IData;
-
-public interface IMemberService extends IData<MemberDTO, RequestMemberDTO, Integer> {
-}
